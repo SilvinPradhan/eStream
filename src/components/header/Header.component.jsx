04f57bb50aa831ec065047ed75f006221c58ed25
Silvin Pradhan <silvinpradhan95@gmail.com>
2021-07-13 23:18:39 -0500

    header home route added

--- src/components/header/Header.component.jsx
+++ src/components/header/Header.component.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import styled from 'styled-components'
-import { withRouter } from 'react-router-dom'
+import { withRouter, Link } from 'react-router-dom'
 
 function Header() {
     return (
         <Nav>
             <Logo src="" />
             <NavMenu>
-
+                <Link to="/">
+                    <img src="" alt="home" />
+                    <span>Home</span>
+                </Link>
             </NavMenu>
         </Nav>
     )
@@ -26,5 +29,20 @@ const Logo = styled.img`
     width: 80px;
 `
 const NavMenu = styled.div`
+    display: flex;
 
+    Link {
+        display: flex;
+        align-items: center;
+        padding: 0 12px;
+
+        img {
+            height: 20px;
+        }
+
+        span {
+            font-size: 13px;
+            letter-spacing: 1.42px;
+        }
+    }
 `
