75a27d8b7557bf6d2767154ee3eab0f0aea17cc2
Silvin Pradhan <silvinpradhan95@gmail.com>
2021-07-14 09:33:09 -0500

    header bar built using styled components

--- src/components/header/Header.component.jsx
+++ src/components/header/Header.component.jsx
@@ -1,17 +1,38 @@
 import React from 'react'
 import styled from 'styled-components'
-import { withRouter, Link } from 'react-router-dom'
+import { withRouter } from 'react-router-dom'
 
 function Header() {
     return (
         <Nav>
-            <Logo src="" />
+            <Logo src="/images/logo.svg" />
             <NavMenu>
-                <Link to="/">
-                    <img src="" alt="home" />
-                    <span>Home</span>
-                </Link>
+                <a href="/">
+                    <img src="/images/home-icon.svg" alt="Home" />
+                    <span>HOME</span>
+                </a>
+                <a href="/">
+                    <img src="/images/search-icon.svg" alt="Search" />
+                    <span>Search</span>
+                </a>
+                <a href="/">
+                    <img src="/images/watchlist-icon.svg" alt="Watchlist" />
+                    <span>Watchlist</span>
+                </a>
+                <a href="/">
+                    <img src="/images/original-icon.svg" alt="Originals" />
+                    <span>Originals</span>
+                </a>
+                <a href="/">
+                    <img src="/images/movie-icon.svg" alt="Movies" />
+                    <span>Movies</span>
+                </a>
+                <a href="/">
+                    <img src="/images/series-icon.svg" alt="Series" />
+                    <span>Series</span>
+                </a>
             </NavMenu>
+            <UserAvatar src="/images/profile.jpg" />
         </Nav>
     )
 }
@@ -30,19 +51,42 @@ const Logo = styled.img`
 `
 const NavMenu = styled.div`
     display: flex;
+    flex: 1;
+    margin-left: 20px;
+    align-items: center;
 
-    Link {
+    a {
         display: flex;
         align-items: center;
         padding: 0 12px;
+        text-decoration: none;
+        color: white;
+        cursor: pointer;
 
         img {
-            height: 20px;
+            height: 25px;
         }
 
         span {
-            font-size: 13px;
-            letter-spacing: 1.42px;
+            font-size: 14px;
+            letter-spacing: 1.5px;
+            position: relative;
+
+            &:after {
+                content: "";
+                height: 2px;
+                background: white;
+                position: absolute;
+                left:0;
+                right: 0;
+                bottom: -6px;
+            }
         }
     }
+`
+const UserAvatar = styled.img`
+    width: 48px;
+    height: 48px;
+    border-radius: 50%;
+    cursor: pointer;
 `
